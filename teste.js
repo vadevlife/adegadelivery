fc35b18b572065a72f53b0063a55491b5775a389
vadevlife <vadevlife@gmail.com>
2022-07-12 17:28:45 -0300

    ..

---
+++ teste.js
@@ -0,0 +1,259 @@
+ 
+
+
+  const allProds = [{  
+     categoria :"Cervejas",
+    "itens":[
+      { 
+        id:'0',
+        name:'Brahma',
+        description:'',
+        price:'R$ 4,00',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'1',
+        name:'Itaipava',
+        description:'',
+        price:'R$ 3,50',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'2',
+        name:'Original',
+        description:'',
+        price:'R$ 6,50',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'3',
+        name:'Heineken',
+        description:'',
+        price:'R$ 6,50',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'4',
+        name:'Imperio e petra',
+        description:'',
+        price:'R$ 4,50',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'6',
+        name:'Long h',
+        description:'',
+        price:'R$ 8,50',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'7',
+        name:'Long bad',
+        description:'',
+        price:'R$ 7,50',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'8',
+        name:'Estela',
+        description:'',
+        price:'R$ 8,50',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'9',
+        name:'Corona',
+        description:'',
+        price:'R$ 9,50',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'26',
+        name:'Itaipava 0%',
+        description:'',  
+        price:'R$ 5,00',
+        img:' ',
+        quantidade: 0,
+      },{ 
+        id:'27',
+        name:'Brahma 0%',
+        description:'',
+        price:'R$ 6,00',
+        img:'hamburgueradc.png',
+        quantidade: 0,
+      },{ 
+        id:'31',
+        name:'Império unidade',
+        description:'',
+        price:'R$ 4,50',
+        img:'calabresa.png',
+        quantidade: 0,
+      }
+  ]
+  },
+  {  
+    categoria:"Whisky",
+    "itens":[
+      { 
+        id:'25',
+        name:'Black Label',
+        description:'',  
+        price:'R$ 140,00',
+        img:' ',
+        quantidade: 0,
+      },{ 
+        id:'31',
+        name:'Famous One whisky',
+        description:'',
+        price:'R$ 25,00',
+        img:'calabresa.png',
+        quantidade: 0,
+      },{ 
+        id:'31',
+        name:'Ballantines',
+        description:'',
+        price:'R$ 99,00',
+        img:'calabresa.png',
+        quantidade: 0,
+      },{ 
+        id:'31',
+        name:'Old star',
+        description:'',
+        price:'R$ 20,00',
+        img:'calabresa.png',
+        quantidade: 0,
+      },{ 
+        id:'31',
+        name:'Passaport',
+        description:'',
+        price:'R$ 55,00',
+        img:'calabresa.png',
+        quantidade: 0,
+      },{ 
+        id:'31',
+        name:'Black stone',
+        description:'',
+        price:'R$ 35,00',
+        img:'calabresa.png',
+        quantidade: 0,
+      }
+  ]  
+ },
+ {  
+  categoria:"Fardo",
+  "itens":[
+    { 
+      id:'31',
+      name:'Fardo Skol',
+      description:'',
+      price:'R$ 36,00',
+      img:'calabresa.png',
+      quantidade: 0,
+    },{ 
+      id:'31',
+      name:'Fardo Brahma',
+      description:'',
+      price:'R$ 38,00',
+      img:'calabresa.png',
+      quantidade: 0,
+    },{ 
+      id:'31',
+      name:'Fardo Heineken',
+      description:'',
+      price:'R$ 59,00',
+      img:'calabresa.png',
+      quantidade: 0,
+    },{ 
+      id:'31',
+      name:'Fardo Original',
+      description:'',
+      price:'R$ 54,00',
+      img:'calabresa.png',
+      quantidade: 0,
+    },{ 
+      id:'31',
+      name:'Fardo Badweiser',
+      description:'',
+      price:'R$ 28,00',
+      img:'calabresa.png',
+      quantidade: 0,
+    },{ 
+      id:'31',
+      name:'Fardo Petra',
+      description:'',
+      price:'R$ 38,00',
+      img:'calabresa.png',
+      quantidade: 0,
+    },{ 
+      id:'31',
+      name:'Fardo Eisenbahn',
+      description:'',
+      price:'R$ 65,00',
+      img:'calabresa.png',
+      quantidade: 0,
+    },{ 
+      id:'31',
+      name:'Fardo Spaten',
+      description:'',
+      price:'R$ 59,00',
+      img:'calabresa.png',
+      quantidade: 0,
+    }
+  ]
+},
+{  
+  categoria:"Vinhos",
+  "itens":[
+    { 
+      id:'12',
+      name:'Vinho Santome',
+      description:'',
+      price:'R$ 22,00',
+      img:'thumb.png',
+      quantidade: 0,
+    }
+  ]
+},
+{  
+  categoria:"Bebidas",
+  "itens":[
+    { 
+      id:'22',
+      name:'Água',
+      description:'Sem Gás e com Gás',
+      price:'R$ 3,00',
+      img:'agua.png',
+      quantidade: 0,
+    },{ 
+      id:'23',
+      name:'Refrigerante Lata',
+      description:'Coca-Cola Lata, Guaraná de 1L.',
+      price:'R$ 6,00',
+      img:'refrilata.png',
+      quantidade: 0,
+    },{ 
+      id:'24',
+      name:'Suco Natural 500ml',
+      description:'Laranja, Limão, Maracujá. ',
+      price:'R$ 9,00',
+      img:'suconatural500.png',
+      quantidade: 0,
+    },{ 
+      id:'25',
+      name:'Suco DEL Valle',
+      description:'Laranja, Limão, Maracujá, Uva.',
+      price:'R$ 7,00',
+      img:'sucodelvalle.png',
+      quantidade: 0,
+    },{ 
+      id:'26',
+      name:'Caldo de Cana 500ml',
+      description:'',
+      price:'R$ 8,00',
+      img:'cana.png',
+      quantidade: 0,
+    }
+]  
+}]
+
+
